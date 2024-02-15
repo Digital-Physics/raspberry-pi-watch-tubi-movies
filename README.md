a6e9c0278b86c145b3e92f97ee5f5d7ba40b379c
Jonathan Khanlian <jdkhanlian@gmail.com>
2024-02-15 10:46:00 -0500

    added youtube functionality

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 ![tubi on raspberry pi](./raspberry_pi_tubi.GIF)
 
-This was some script to watch movies on tubi after SSH into the Raspberry Pi.
+This is some script to watch movies on tubi and youtube. It is helpful if you SSH into your Raspberry Pi (with wifi capabilities).
 
-Once in the raspberry pi, from the terminal launch this file with "python main.py". 
-Make sure you have "cd" to the right directory and have any packages installed that are needed.
+Once in the raspberry pi terminal, navigate to where you have saved this file and run it with "python main.py". 
+
+Update the the movie URL in the file as desidre on your Raspberry Pi using "nanno main.py"
 
 Audio Note: Static was encountered on our Bluetooth speaker that was connected to the Raspberry Pi 4 model b.
-To avoid that, open the config file with "sudo nano /boot/config.txt" and add this line to the config.txt file:
+To avoid it, open the config file with "sudo nano /boot/config.txt" and add this line to the config.txt file:
 "audio_pwm_mode=2"
 
-Code Note: This code is somewhat dependent on the current UI layout of the tubi site, so you may need to update it.
+Code Note: This code is somewhat dependent on the current tubi and youtube UI, so you may need to update it if things change.
 
 
 
--- main.py
+++ main.py
@@ -5,38 +5,49 @@ from selenium.webdriver.common.keys import Keys  # Import Keys class
 from selenium.webdriver.common.by import By
 import os
 import time
+import random
 
 os.environ['DISPLAY'] = ':0' # to avoid error when ssh
-os.system("amixer set Master 60%")  # Linux way to set the audio
+os.system("amixer set Master 50%")  # Linux way to set the audio
 
-# url = "https://tubitv.com/movies/100005567/cosmodrama"
-url = "https://tubitv.com/movies/100015091/digital-physics"
 chrome_service = Service('/usr/lib/chromium-browser/chromedriver')
 driver = webdriver.Chrome(service=chrome_service)
 
-driver.get(url)
+site = "youtube" if random.random() < 0.2 else "tubi"
 
-time.sleep(4)
+if site == "youtube":
+  url = "https://www.youtube.com/watch?v=9uVKwhU2lp0&t=40s"
 
-play_btn = driver.find_element(By.CSS_SELECTOR, "div.web-iconButton")
-play_btn.click()
+  driver.get(url)
+  time.sleep(4)
+  
+  # play
+  body = driver.find_element(By.TAG_NAME, 'body')
+  body.send_keys(Keys.ARROW_UP + " ")
+ 
+  # full-screen
+  body.send_keys("f")
+elif site == "tubi":
+  url = "https://tubitv.com/movies/100015091/digital-physics"
+  
+  driver.get(url)
+  time.sleep(4)
+  
+  #play
+  play_btn = driver.find_element(By.CSS_SELECTOR, "div.web-iconButton")
+  play_btn.click()
 
-# Wait for the video to start playing
-time.sleep(4)
+  #full-screen button
+  full_screen_btn = driver.find_element(By.CSS_SELECTOR, "span#fullscreenArea button.web-iconButton-container")
+  full_screen_btn.click()
 
-# Find and click the full-screen button
-full_screen_btn = driver.find_element(By.CSS_SELECTOR, "span#fullscreenArea button.web-iconButton-container")
-full_screen_btn.click()
+  # jump ahead
+  jump = driver.find_element(By.TAG_NAME, 'body')
+  #for _ in range(5):
+    #time.sleep(1)
+    #jump.send_keys(Keys.ARROW_DOWN + 'l') # jump 30 seconds ahead
 
-time.sleep(3)
-
-jump = driver.find_element(By.TAG_NAME, 'body')
-
-# modify this if you want to skip ahead in the movie
-for _ in range(5):
-  time.sleep(1)
-  # jumps 30 seconds
-  jump.send_keys(Keys.ARROW_DOWN + 'l')
-
+# don't let this script complete or your webdriver will go away
 while True:
   pass
+
